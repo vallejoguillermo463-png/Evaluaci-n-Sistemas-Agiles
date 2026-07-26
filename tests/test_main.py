cfb4cc84b30684377db414410539fac86e8d277e
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-07-25 22:06:16 -0500

    [ADD] Agregar archivo .gitignore para excluir archivos temporales

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+from gestor_estudiantes import (
+    validar_nota,
+    calcular_suma,
+    determinar_estado,
+)
+
+
+def test_validar_nota_correcta():
+    assert validar_nota(8) is True
+
+
+def test_validar_nota_incorrecta():
+    assert validar_nota(12) is False
+
+
+def test_calcular_suma():
+    assert calcular_suma(8, 8, 8) == 24
+
+
+def test_estado_aprobado():
+    assert determinar_estado(24) == "APROBADO"
+
+
+def test_estado_reprobado():
+    assert determinar_estado(18) == "REPROBADO"

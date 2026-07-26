60106481d1794ad61c822d670379602311e19cb2
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-07-25 21:53:42 -0500

    [UPDATE] Reubicar la lógica principal en gestor_estudiantes

--- gestor_estudiantes.py
+++ gestor_estudiantes.py
@@ -1,3 +1,4 @@
+estudiantes = []
 NOTA_MINIMA_APROBACION = 24
 
 def validar_nota(nota):
@@ -20,5 +21,63 @@ def determinar_estado(suma):
         return "APROBADO"
     return "REPROBADO"
 
+def registrar_estudiante(nombre, nota1, nota2, nota3):
+    if not validar_nombre(nombre):
+        return
 
+    if not validar_nota(nota1):
+        return
 
+    if not validar_nota(nota2):
+        return
+
+    if not validar_nota(nota3):
+        return
+
+    suma = calcular_suma(nota1, nota2, nota3)
+
+    estado = determinar_estado(suma)
+
+    estudiantes.append({
+        "nombre": nombre,
+        "nota1": nota1,
+        "nota2": nota2,
+        "nota3": nota3,
+        "suma": suma,
+        "estado": estado
+    })
+
+    print("Estudiante registrado")
+
+def listar_estudiantes():
+    if len(estudiantes) == 0:
+        print("No existen estudiantes")
+    else:
+        print("LISTA DE ESTUDIANTES")
+        for e in estudiantes:
+            print(
+                e["nombre"],
+                e["nota1"],
+                e["nota2"],
+                e["nota3"],
+                e["suma"],
+                e["estado"]
+            )
+
+def buscar_estudiante(nombre):
+    encontrado = False
+
+    for e in estudiantes:
+        if e["nombre"] == nombre:
+            print(
+                e["nombre"],
+                e["nota1"],
+                e["nota2"],
+                e["nota3"],
+                e["suma"],
+                e["estado"]
+            )
+            encontrado = True
+
+    if encontrado == False:
+        print("Estudiante no encontrado")
--- main.py
+++ main.py
@@ -1,75 +1,8 @@
-estudiantes = []
-NOTA_MINIMA_APROBACION = 24
 from gestor_estudiantes import (
-    validar_nota,
-    validar_nombre,
-    calcular_suma,
-    determinar_estado,
+    registrar_estudiante,
+    listar_estudiantes,
+    buscar_estudiante,
 )
-
-def registrar_estudiante(nombre, nota1, nota2, nota3):
-    if not validar_nombre(nombre):
-        return
-
-    if not validar_nota(nota1):
-        return
-
-    if not validar_nota(nota2):
-        return
-
-    if not validar_nota(nota3):
-        return
-
-    suma = calcular_suma(nota1, nota2, nota3)
-
-    estado = determinar_estado(suma)
-
-    estudiantes.append({
-        "nombre": nombre,
-        "nota1": nota1,
-        "nota2": nota2,
-        "nota3": nota3,
-        "suma": suma,
-        "estado": estado
-    })
-
-    print("Estudiante registrado")
-
-
-def listar_estudiantes():
-    if len(estudiantes) == 0:
-        print("No existen estudiantes")
-    else:
-        print("LISTA DE ESTUDIANTES")
-        for e in estudiantes:
-            print(
-                e["nombre"],
-                e["nota1"],
-                e["nota2"],
-                e["nota3"],
-                e["suma"],
-                e["estado"]
-            )
-
-
-def buscar_estudiante(nombre):
-    encontrado = False
-
-    for e in estudiantes:
-        if e["nombre"] == nombre:
-            print(
-                e["nombre"],
-                e["nota1"],
-                e["nota2"],
-                e["nota3"],
-                e["suma"],
-                e["estado"]
-            )
-            encontrado = True
-
-    if encontrado == False:
-        print("Estudiante no encontrado")
-
 
 registrar_estudiante("Ana", 8, 8, 8)
 registrar_estudiante("Luis", 6, 6, 6)
